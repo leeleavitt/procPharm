7ceb0ee53bebfe584a3cf49ca6fb2716c2408a12
leeWork <lee.leavitt.u@gmail.com>
2019-10-07 16:37:00 -0600

    Updated so when a cell is dropped in tcd by pressing x it will also be removed from all cell types

--- cell_type_checker.r
+++ cell_type_checker.r
@@ -20,10 +20,35 @@ cellTypesToClean <- setdiff(cellTypeNames,c('neurons','glia'))
 
 #FOR EACH CELL TYPE
 i = 3
-cat('\n Please scan cell type ',names(rdTmp[[cellTypeId]][i]),'\n' )
-cat('Place the cells to drop into key 1, You can always redo this by pressing shift+ that number\n')
+cat('\n Please correct cell type ',names(rdTmp[[cellTypeId]][i]),'\n' )
+cat('Place the cells to drop into key 1, You can always redo this by pressing shift+that number\n')
+#prep the tcd input
 drops <- c()
 forTcd <- list(drops, rdTmp[[cellTypeId]][[i]])
 names(forTcd) <- c('drops', names(rdTmp[[cellTypeId]][i]))
+#scan the cells and add to the drop
+cellsToDrop <- tcd(rdTmp, forTcd, save_question = F)
+
+#Now that we have dropped cells, figure out where to place them in each cell type
+#Realizing that i need to update procpharm i am am going to now go to procpharm to update this in tcd
+if( length(cellsToDrop[[1]]) > 0 ){
+
+}
+
+###
+#Lets play with removing a cell from all groups
+cellToRemove <- "X.49"
+
+rdTmp[[cellTypeId]][cellTypesToClean] <- lapply(rdTmp[[cellTypeId]][cellTypesToClean], function(X) setdiff(X, cellToRemove))
+
+
+
+
+
+
+
+
+
+
 
 
