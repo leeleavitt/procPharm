8171f3c90a5306016bfc4bcbb4d9e281ef953cc9
LeeHome <lee.leavitt.u@gmail.com>
2020-04-06 22:40:40 -0600

    hmm

--- R/zzz.R
+++ R/zzz.R
@@ -3,16 +3,14 @@
 .onLoad <- function(libname, pkgname){
     systemType <- Sys.info()[1]
     if( systemType != "Windows" ){
-        formals(tcd)$info <- F
-        formals(tcd)$bcex <- 0.5
+        formals(tcd)$info <<- F
+        formals(tcd)$bcex <<- 0.5
         
         windows <<- cairoDevice::Cairo
-        formals(windows)$pointsize <- 9
-        formals(windows)$xpos <- NULL
-        formals(windows)$ypos <- NULL
+        formals(windows)$pointsize <<- 5
         
-        formals(PeakFunc7)$bcex <- 1.5
-        formals(RDView)$wh <- 11
-        formals(RDView)$hh <- 6
+        formals(PeakFunc7)$bcex <<- 1.5
+        formals(RDView)$wh <<- 11
+        formals(RDView)$hh <<- 6
     }
 }
