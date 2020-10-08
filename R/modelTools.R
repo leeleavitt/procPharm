e161bec3717860c97cee0585aa4ce897ef5904f5
Lee Leavitt <lee.leavitt.u@gmail.com>
2020-10-08 12:07:02 -0600

    adding a lungmodeler fo rth cancer project

--- R/modelTools.R
+++ R/modelTools.R
@@ -673,3 +673,119 @@ modelViewer <- function(dat, cell, plot.new = T){
 
 
 }
+
+#' Function to apply a general purpose potassium model which was trained on a potassium 
+#' dose response experiment. This will apply the model to any potassium window
+#' except the first k40 window, which has its own model to use
+#' @export
+potassiumModeler <- function(dat){
+    model <- keras::load_model_hdf5("Y:/Computer Setup/R/models/kdr.h5")
+    pyPharm <- reticulate::import('python_pharmer')
+
+    windowSize <- 3
+    featureWindows <- 12
+    tType <- c("blc")
+
+    # Here we are collecting the windows that were scored
+    # These are all the smaller windows that were ensured to be correct
+    wr <- dat$w.dat$wr1
+    levs <- setdiff(unique(dat$w.dat$wr1), "")
+    kWins <- grep("^[kK]", levs, value = T)
+    cellTypeK40 <- grep("^[kK][.]40", levs, value = T)[1]
+    kWins <- setdiff(kWins, cellTypeK40)
+
+    x1s <- tapply(dat$w.dat[,"Time"],as.factor(wr),min)
+    x2s <- tapply(dat$w.dat[,"Time"],as.factor(wr),max)
+    windowLen <- x2s - x1s
+    windowLen <- windowLen[kWins]
+
+    winCol <- names(windowLen[windowLen < 2 & windowLen > 1.2])
+
+    winStart <- sort(x1s[winCol])
+    winEnd <- winStart + windowSize
+
+    # Make an uncertainty matrix, or collect it.
+    if(is.null(dat$uncMat)){
+        uncertainMat <- data.frame(matrix(nrow = dim(dat$c.dat)[1], ncol = 0))
+        row.names(uncertainMat) <- row.names(dat$c.dat)
+    }else{
+        uncertainMat <- dat$uncMat
+    }
+
+    #Apply the model to everything to see how well it performs
+    for(i in 1:length(winStart)){
+        windowLogic <-  dat$w.dat$Time > winStart[i] & 
+                        dat$w.dat$Time < winEnd[i]
+
+        pulseToScore <- as.data.frame(t(dat[[tType]][windowLogic,-1]))
+        featureFrame <- pyPharm$featureMaker2(pulseToScore, featureWindows)
+        
+        dat$bin[,names(winStart)[i]] <- model$predict_classes(featureFrame)
+
+        # add to the uncMat
+        probs <- model$predict(featureFrame)
+        uncertainty <- sqrt(probs[,1]^2 + probs[,2]^2)
+        uncertainMat[names(winStart)[i]] <- uncertainty
+        
+    }
+
+    dat$uncMat <- uncertainMat
+
+    return(dat)
+}
+
+
+#' Function to apply a general purpose potassium model which was trained on a potassium 
+#' dose response experiment. This will apply the model to any potassium window
+#' except the first k40 window, which has its own model to use
+#' @export
+lungModeler <- function(dat){
+    model <- keras::load_model_hdf5( "Y:/Computer Setup/R/models/lungMulti.h5")
+    pyPharm <- reticulate::import('python_pharmer')
+
+    windowSize <- 4
+    featureWindows <- 12
+    tType <- c("blc")
+
+    # Here we are collecting the windows that were scored
+    # These are all the smaller windows that were ensured to be correct
+    wr <- dat$w.dat$wr1
+    levs <- setdiff(unique(dat$w.dat$wr1), c("", 'epad'))
+
+    x1s <- tapply(dat$w.dat[,"Time"],as.factor(wr),min)
+    x2s <- tapply(dat$w.dat[,"Time"],as.factor(wr),max)
+    windowLen <- x2s - x1s
+    windowLen <- windowLen[levs]
+
+    winStart <- sort(x1s[levs])
+    winEnd <- winStart + windowSize
+
+    # Make an uncertainty matrix, or collect it.
+    if(is.null(dat$uncMat)){
+        uncertainMat <- data.frame(matrix(nrow = dim(dat$c.dat)[1], ncol = 0))
+        row.names(uncertainMat) <- row.names(dat$c.dat)
+    }else{
+        uncertainMat <- dat$uncMat
+    }
+
+    #Apply the model to everything to see how well it performs
+    for(i in 1:length(winStart)){
+        windowLogic <-  dat$w.dat$Time > winStart[i] & 
+                        dat$w.dat$Time < winEnd[i]
+
+        pulseToScore <- as.data.frame(t(dat[[tType]][windowLogic,-1]))
+        featureFrame <- pyPharm$featureMaker2(pulseToScore, featureWindows)
+        
+        dat$bin[,names(winStart)[i]] <- model$predict_classes(featureFrame)
+
+        # add to the uncMat
+        probs <- model$predict(featureFrame)
+        uncertainty <- sqrt(probs[,1]^2 + probs[,2]^2)
+        uncertainMat[names(winStart)[i]] <- uncertainty
+        
+    }
+
+    dat$uncMat <- uncertainMat
+
+    return(dat)
+}
