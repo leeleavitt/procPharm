f256f0109a3d84ccb8bdf73987dea37836d5751e
Arikill <ralluri.utah@gmail.com>
2020-10-30 15:15:17 -0600

    server setup and test.

---
+++ serverClientFramework/routers.R
@@ -0,0 +1,8 @@
+# router.R
+# Identifies incomming requests and routes them to R functions.
+
+#* @param msg The message to echo back.
+#* @get /echo
+function(msg="") {
+    list(msg = paste0("The test message is: '", msg, "'"))
+}
